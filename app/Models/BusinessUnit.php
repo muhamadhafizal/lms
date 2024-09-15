b60dbcc51ca2c2290048fe0d581e2069d43f0d43
Ijal <ijaldev16@gmail.com>
2024-09-15 10:05:16 +0800

    [UserManagement][Profiles]:Model and migration files

---
+++ app/Models/BusinessUnit.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class BusinessUnit extends Model
+{
+    use HasFactory, LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly([
+            'name',
+            'code',
+            'description',
+            'active',
+        ])
+        ->useLogName('business_unit')
+        ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName}");
+        // Chain fluent methods for configuration options
+    }
+
+    protected $fillable = [
+        'id',
+        'name',
+        'code',
+        'description',
+        'active',
+    ];
+}
---
+++ app/Models/Religion.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Religion extends Model
+{
+    use HasFactory, LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly([
+            'name',
+            'code',
+            'description',
+            'active',
+        ])
+        ->useLogName('religion')
+        ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName}");
+        // Chain fluent methods for configuration options
+    }
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'active',
+    ];
+}
