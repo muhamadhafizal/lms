e097d59bd00cd67ae868a9b22f9119c3d3a5378f
Karu <shafiqhaikal1763@gmail.com>
2025-04-24 16:11:06 +0800

    merged from dev

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,20 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-<<<<<<< HEAD:app/Models/Books.php
-    
-    protected $fillable = [
-        'title',
-        'isbn',
-        'author',
-        'status'
-=======
 
     protected $fillable = [
         'title',
         'author',
         'isbn',
         'status',
->>>>>>> 65f1cb32dbafc96bf2c761253b3ec1980c30dbc7:app/Models/Book.php
     ];
 }
